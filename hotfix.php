d284eeacd9a0ddd3520cfd7d3a82e553ed23ea6f
Trinidad Moruga Scorpion <phixpc@protonmail.com>
2026-01-08 12:09:03 +0200

    http://datat.freehostia.com/hotfix.php
    http://datat.freehostia.com/hotfix.php
    
    http://datat.freehostia.com/hotfix.php
    
    http://datat.freehostia.com/hotfix.php
    
    http://datat.freehostia.com/hotfix.php

---
+++ hotfix.php
@@ -0,0 +1,58 @@
+<html>
+<center>
+	<body>
+<h1 style="background-color:Yellow;">reversing back to history manpage.</h1>
+<br><br>
+<title>Fix sudoers.conf owned by uid error fix. Use by running terminal: "php uid_fix.php". You might need to run "sudo apt install php", or "php8.3" or "php5" as endpoint.", and before execute this hotfix.
+</title><br><br>
+<h1 style="background-color:hsla(9, 100%, 64%, 0.5);">hsla(9, 100%, 64%, 0.5) trying to make echo from magnetic balance in survival physic's. Ai musta et oo kuullu vai???</
+<br>
+<p>Does the under top have more power??</p><br><BR>
+<h1 style="background-color:hsla(14, 14%, 14%,8.5);">STEGSEEK AND STEGHIDE AND NEXT PART IS .MP4 FILE USING BASE64_UPLOAD SOURCE CODE YOU CAN REVERSE ENGINEER TO ATTACH THE MEDIA TO WORKBENCH, ETC HEXEDIT..</h1>
+<font size="32" color="pink">
+<br><br><b>Fix sudoers.conf owned by uid error fix. Use by running terminal: "php uid_fix.php". You might need to run "sudo apt install php", or "php8.3" or "php5" as endpoint.", and before execute this hotfix.</b>
+<BR><img src="combine_head.jpg" width="420" height="339"><p>Use same image file in github repository and here. Dont rename the original filename which was: combine_head.jpg, just wrote file name after domain root. Hotfix does not have has pointed to directory, just the repository file tree. Drop verify_from_github_original_source_code_or_just_use_diff_to_check_files_are_ident.jpg</p>
+<a href="Pictures">This is the link to fill the must have at least one link on webppage. Originally this file is hotfix for uid error at sudoers.conf via terminal output. Next time remember set --debug option to the metadata or embed it to the head file that you might start findings from this redirection from original page. Maybe there is some releases to not make invisible man using electric fields to use this text when we have, let's take a look. About 420 lines to text fill and head says empty. Do it again and again. So it might be the repeating technigue to wait when something changes. Then just give time for CPU and use diffutils to determine what is calculated reality between 2 different places on same verification key should i say. Allowing to add 1 more link to the site, check the Copyright Lisence for admin uploads to share the key is plastic and will be on eye contact visible to handle the full secrets.data.enc file that i promise to put to the pictures dir, but without input first. Ok i allow to make  public feedback form for replies visible for everyone, so you see your feedback as plaintext fun when get base64 handle. So your output is not visible by readable form, you need to use key number one i mentioned before this midnight cruiser. What i need, just 4 VPS servers, VPN  and .com domain public form to share short urls for free. To make this possible check donate folder at root directory of this magic domain. Domain is: datat.freehostia.com and uses SSL certificate so you had to put https:// instead of just http:// for firestarter. While stopping this instance now i do the uploads for files i  promised to my domain and free some space if needed. Lastly, dont use cryptography when using php source code to release. It will put your md5 hash hidden and exploitable to the directory you just created for php cryptography. I suggest just use openssl at linux terminal on local machine before going forward. The next form is saved and only you have the key. There are things called trackers, so they might log your ip address and gather some information about your visiting by  pointing database creation to your ip address so they can identify you. Big mama dynamite is  watching be ready.
+
+	Hola Senor!
+
+Yours sincerely,
+TMS you might find me on irc.libera.chat that would be elite for as victim, but as host it uses plaintext passwords without no encoding and passwords are stored on mysql database i suggest. Dont be sure are they on MD5 form. Maybe try to get their database and see on my own. That could be be sure for get not ddoxxed and use allways VPN when doing things that might save peoples security on growning internet that has no data limit and it reaches the all possible ipv4 addresses are just used mostly, then they get the next one, the ipv9/0x90/N8.
+
+The real limit is the data storage space limit that has allways numbers and numbers, newer ending JustRunTheCooperNOP to get more space to create the void from empty paper with one colour penicil at start. That might lead to the cornel. These services i had released are open source and using GNu Lisence, that i haven't read from start to end, but just want not to wrote own lisence for copyrights, because í dont know nothing. Is the earth cornel and space or is mind regenerating followers. My baddest thing is to put anonymous hit counter to my homepage and it has hacked by Evergreen Terrace Cowboys Hacker group or the Doodles Delphin Elite group.
+
+I can share, but you have to join the pieces to build the key to my art to see it like i wanted to people have not so easy way to select. You might not have time to read this text. But the idea is fill just 24 lines, but one line contains 20 words. We can split if they have upload limit and even file limit. BurpSuite has be the best friend for starting to understand that data is invisible before mime type is set.
+
+I suggest openssl to encrypt your important things. These are just for make to solve the think 1000 words text input without copy paste and using just your own brain. You can use this text as 255,15,18 RGB setting, then i know you really did read the text, not just copied it. So set font color to that scope without no space to change the key that is in RGB colour format and can be spelled as 255015018 and even could be used as password if i go back to the history when setting new password for by get hacked again and ddoss attacks repeats usually the same send data to server like "¤#%¤#%#/#¤#¤&#435345dsfsd" in nutshell, so if you get scope on the things repeat on yours server you can block the request if its same after 5 times to send to port 80 (http) or why not to 443(HTTPS). Dont know does SSL certificates block the attack by on their own. So i suggest to use my https domain starts as words "https://datat.", to do list is to remove the other dy.fi domains, because has no static or was it dynamic ip address. I am behind NAT web and cannot share because as we say the hardware firewall. You can set the APN or somehow setting to different or just guess and why not try to different operator internet settings to get out from nat limits and rules that are making firejail command to possible to exec. Okay so this is the end this was the html part of this program. Wanted just add some colours, few links and why not few pictures to watch on the same when i release the hotfix to the sudoers.conf error on linux terminal debug bridge.
+
+We can use public feedback form as we communicate. It will need you to use Swiss Army Knife tool base64 that is installed on most linux systems by the kernel image.
+
+The original file name was: check the page source to remove scale limits to save you from browser crash. The other ddos particular is download from server, like set the POST HTTP REQUEST to get server to have more work that the command takes to send. So it is like in my math 12/50 and server is the bigger data user on this sceneration. Can be blocked to set data limit to download from server for the each ip address which tries to download something from your server. Best way is block this data download limit to same on every open port. Like 80MB/12hours computer can release the data for individuals.
+
+I ḱnow also bypass to this but it is released when you dig little deeper on this "hotfix source code release" which was the firestarter. Jump to point dots to gether Adios Amigos - TMS
+
+
+</a><BR>
+</font>
+<br><br>
+<font size="32" color="purple">
+<br><b>Repeat the wings of a butterfly. Fix sudoers.conf owned by uid error fix. Use by running terminal: "php uid_fix.php". You might need to run "sudo apt install php", or "php8.3" or "php5" as endpoint.", and before execute this hotfix.</b><br><br>
+<p>linux error msg: "/etc/sudoers.conf owned by uid 1000 and should be 0 error fix"</p><br><br>
+<?php
+
+system("sudo test");
+
+system("sudo apt purge sudo");
+
+system("sudo apt install sudo");
+
+system("sudo test | tee for_verify_that_error_is_fixed_by_these_2_sudo_char_set_commands_this_file_must_be_empty_to_get_hotfix_repair_done_if_this_file_conttains_no_repeated_errr_then_success_to_repair_sudoers.conf.txt");
+
+
+echo "<br><br>You should get error once, then error is fixed.<br>";
+
+?>
+</center>
+</font>
+</body>
+<html>
